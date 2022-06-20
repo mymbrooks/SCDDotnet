2b6a2dfc867b9becf3bf2d15c9c675ae3f197587
wangsen <wangsen@ruibu.info>
2022-06-20 20:43:53 +0800

    完善项目，准备自动生成合同

---
+++ Server/Models/domain/Company.cs
@@ -0,0 +1,7 @@
+﻿namespace Server.Models.domain
+{
+    public class Company
+    {
+
+    }
+}
---
+++ Server/Models/domain/Project.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Server.Models.domain
+{
+    [Table("project", Schema = "public")]
+    public class Project
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int? id { get; set; }
+        public string number { get; set; }
+        public string name { get; set; }
+        public int? projecttypeid { get; set; }
+        public int? entrusttypeid { get; set; }
+        public int? companyid { get; set; }
+        public int? entrustcustomerid { get; set; }
+        public int? clientcustomerid { get; set; }
+        public decimal? totalamount { get; set; }
+        public decimal? prepayamount { get; set; }
+        public decimal? remainingamount { get; set; }
+        public int? reportprovidetypeid { get; set; }
+        public int? reportcount { get; set; }
+        public int? reportday { get; set; }
+        public DateTime? reportprovidedate { get; set; }
+        public DateTime? begindate { get; set; }
+        public DateTime? enddate { get; set; }
+        public bool? isinvoice { get; set; }
+        public int? invoicetypeid { get; set; }
+        public bool? isallowsubcontract { get; set; }
+        public bool? isurgent { get; set; }
+        public bool? iscriticize { get; set; }
+        public string positionmapurl { get; set; }
+        public int? createuserid { get; set; }
+        public DateTime? createtime { get; set; }
+        public int? stateid { get; set; }
+        public string remark { get; set; }
+    }
+}
